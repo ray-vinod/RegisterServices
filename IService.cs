d3f900711468f377488d2677705b347bb443cd54
ray-vinod <ray_vinod@live.com>
2024-04-04 14:30:38 +0545

    IConfiguration can inject by constructor and update readme file

--- IService.cs
+++ IService.cs
@@ -1,9 +1,8 @@
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RegisterServices;
 
 public interface IService
 {
-    void Services(IServiceCollection services,IConfiguration configuration);
+    void Services(IServiceCollection services);
 }
